80d67e51fef6bbcdd8b9aaf8414c0660f95a7935
Giap <tvgsoft@gmail.com>
2017-02-24 20:58:58 +0700

    Test CI process.

--- Example/Example/SourceViewController.swift
+++ Example/Example/SourceViewController.swift
@@ -53,14 +53,4 @@ class SourceViewController: UIViewController {
         EventBus.shared.postSticky(name: .TargetMessage, object: "Hello Target View")
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
